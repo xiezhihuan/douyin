552463ce6bbaee527832d56eb9554f307f74a4af
xiezh <602666178@qq.com>
2020-04-10 22:44:20 +0800

    封装mongo数据库的读写操作（结合async/await和promise实现异步）

--- common/db.js
+++ common/db.js
@@ -1,34 +1,83 @@
-const MongoClient = require('mongodb').MongoClient;
-const test = require('assert');
+const mongodb = require("mongodb");
+const assert = require('assert');
+const mongoClient = mongodb.MongoClient;
 
 // Connection url
 const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'douyin';
-const collectionName = 'crude_hot_word';
+module.exports.collectionName = {
+    crude_hot_word: 'crude_hot_words',
+    crude_hot_word_haveId:'crude_hot_words_haveId',
+    crude_hot_video:"crude_hot_videos",
+    hot_words_haveId:'hot_words_haveId',
+    hot_words:'hot_words_haveId',
+    hot_videos:'hot_videos'
+};
 
-class DB {
-    constructor(url, dbName, collectionName) {
-        this.url = url;
-        this.dbName = dbName;
-        this.collectionName = collectionName
-    }
 
-    save(data) {
-        // Connect using MongoClient
-        MongoClient.connect(url, function (err, client) {
-            test.equal(null, err);
-            // Create a collection we want to drop later
-            const col = client.db(dbName).collection(collectionName);
-            // Insert a bunch of documents
-            col.insertOne(data, {w: 1}, function (err, result) {
-                test.equal(null, err);
-                client.close();
-            });
+
+
+function _connect(){
+    return new Promise((resolve,reject)=>{
+        mongoClient.connect(url,{ useUnifiedTopology: true },(err,client) => {
+            assert.equal(null,err,'数据库连接失败');
+            const db = client.db(dbName);
+            resolve(db);
+            // reject(err);
         });
-    }
+    });
 }
 
-var db = new DB(url,dbName,collectionName);
-module.exports = db;
+module.exports.insertOne = async (collName,insertObj)=>{
+    const db = await _connect();
+    return new Promise((resolve,reject)=>{
+        db.collection(collName).insertOne(insertObj,(err,results)=>{
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+};
+
+// 根据条件查找数据，返回查找到的数量
+module.exports.count = async (collName,whereObj={})=>{
+    const db = await _connect();
+    return new Promise((resolve,reject)=>{
+        db.collection(collName).countDocuments(whereObj,(err,results)=>{
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+};
+// 根据条件查找数据（查找之前进行排序、跳过、查找几条等操作），返回查找到的数据
+module.exports.find = async (collName,whereObj={},limit=0,skip=0,sortObj={})=>{
+    const db = await _connect();
+    return new Promise((resolve,reject)=>{
+        db.collection(collName).find(whereObj).sort(sortObj).skip(skip).limit(limit).toArray((err,results)=>{
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+};
+// 根据id查找到该数据并进行修改
+module.exports.updateOneById = async (collName,id,updateObj)=>{
+    const db = await _connect();
+    return new Promise((resolve,reject)=>{
+        db.collection(collName).updateOne({_id:mongodb.Objectid(id)},updateObj,(resolve,reject)=>{
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+};
+
+// 根据id查找到数据并进行删除操作
+module.exports.deleteOneById = async (collName,id)=>{
+    const db = await _connect();
+    return new Promise((resolve,reject)=>{
+        db.collection(collName).deleteOne({_id:mongodb.ObjectId(id)},(err,results)=>{
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+};
