13ed7d7117b77365ce58b59b4e26fb842e0cf66b
xiezh <602666178@qq.com>
2020-04-11 23:44:22 +0800

    完善抖音爬虫代码，爬去每日热搜词、视频，保存原始数据，且整理得到热搜词库，视频库。

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const douyin = require('./work/douyin');
 
 async function run() {
-    douyin.getTodayHotVideos().then(()=>{
-        console.log('完成每日热搜视频更新');
+   updateHotwords();
+   douyin.getHotVideos();
+}
+
+function updateHotwords() {
+    douyin.getExactHotWord_haveId().then(()=>{
+        console.log('完成热搜词精确库更新（有id的）');
     });
-    douyin.getTodayHotWords().then(()=>{
-        console.log('完成每日热搜词更新');
+    douyin.getExactHotWord().then(()=>{
+        console.log('完成热搜词精确库更新（无id的）');
     });
-    const hotWords = await douyin.getTodayHotWords_haveId().then((hotWords)=>{
-        console.log('完成每日热搜词更新（无groupid）');
-        return hotWords;
-    });
-    // douyin.mergeHotWordDoc_haveId(hotWords.data.data.word_list).then(()=>{
-    //     console.log('完成更新热搜词库');
-    // });
 }
 
 run();
--- work/douyin.js
+++ work/douyin.js
@@ -3,6 +3,7 @@ var dbUtils = require('../common/dbUtils');
 var Url = require('../common/url');
 const agent = require('../common/http/agent');
 
+
 async function _getHotSearch(url,collection) {
    const resp = await agent.get(url);
    const data ={
@@ -10,44 +11,56 @@ async function _getHotSearch(url,collection) {
         timestamp: new Date().getTime(),
         data: resp,
    };
-   dbUtils.insertOne(collection,data);
+   dbUtils.updateOneByField(collection,{date:data.date},data);
    return data;
 }
 
 const douyin = {
-
-    /*获取今日热搜词(词更多，但没有groupid)
-    * 频率：每日一次，23：00更新
+    /*
+    * 获取精确的热搜词（含id），并存入数据库
+    * 包含这三个字段{hot_value,group_id,word}
     * */
-    getTodayHotWords(){
-        return _getHotSearch(Url.hot_word_list,dbUtils.collectionName.crude_hot_word);
-    },
-
-    /*获取今日热搜词（词少，但有groupid）
-    * 频率：每日一次，23：00更新
-    * */
-    getTodayHotWords_haveId(){
-        return _getHotSearch(Url.hot_word_list_haveId,dbUtils.collectionName.crude_hot_word_haveId);
-    },
-
-    /*获取今日热搜视频
-    * 频率：每日一次，23：00更新
-    * */
-    getTodayHotVideos(){
-       return  _getHotSearch(Url.hot_video_list,dbUtils.collectionName.crude_hot_video);
-    },
-
-   async mergeHotWordDoc_haveId(todayData){
-       var arr = await dbUtils.find(dbUtils.collectionName.hot_words_haveId);
-       var doc = arr[0];
-       if (!arr[0]) doc = {}; //避免为空时，doc不是对象
-       //将今日的数据合并到总表中
-        todayData.forEach((item)=>{
-            //通过对象key的唯一性去重，确保每条热搜词唯一
-            doc[item.group_id] = {name: item.word, hot_value: item.hot_value};
+   async getExactHotWord_haveId(){
+        const crudeHotWord = await _getHotSearch(Url.hot_word_list_haveId,dbUtils.collectionName.crude_hot_word_haveId);
+       const wordList = crudeHotWord.data.data.word_list;
+        wordList.forEach(item =>{
+            const {hot_value,group_id,word} = item;
+            dbUtils.updateOneByField(dbUtils.collectionName.hot_words,{word},{hot_value,group_id,word})
         });
-        dbUtils.insertOne(dbUtils.collectionName.hot_words_haveId,doc);//todo 是先删除再插入，还是刷新，还是其他方法？
+    },
+
+    /*
+    * 获取精确的热搜词（含id），并存入数据库
+    * 包含这三个字段{hot_value,group_id,word}
+    * */
+    async getExactHotWord(){
+        const crudeHotWord = await _getHotSearch(Url.hot_word_list,dbUtils.collectionName.crude_hot_word);
+        const wordList = crudeHotWord.data.word_list;
+        wordList.forEach(item =>{
+            const {hot_value,word} = item;
+            dbUtils.updateOneByField(dbUtils.collectionName.hot_words,{word},{hot_value,word})
+        });
+    },
+
+    /*
+    * 从热搜视频数据中筛选有用字段，并存入视频库中*/
+    async getHotVideos(){
+        const crudeVideosObj = await _getHotSearch(Url.hot_video_list,dbUtils.collectionName.crude_hot_video);
+        const aweme_list = crudeVideosObj.data.aweme_list;
+        aweme_list.forEach(item =>{
+            const {share_url,aweme_id,create_time,desc,hot_value,group_id,duration,} = item.aweme_info;
+            const {vid} = item.aweme_info.video;
+            const cover = item.aweme_info.video.cover.url_list[0];
+            const video = item.aweme_info.video.play_addr.url_list[0];
+            dbUtils.updateOneByField(dbUtils.collectionName.hot_videos,{vid},{vid,desc,hot_value,
+                video,cover,create_time,duration,share_url,group_id,aweme_id})
+        })
     }
+
+
+
+
+
 };
 
 module.exports = douyin;
